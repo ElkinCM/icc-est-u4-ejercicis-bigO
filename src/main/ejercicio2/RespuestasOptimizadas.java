1b82c4d5c7bc28cbf975ee8b2a8d4bce76112e0f
EMCMateo <mateo5678moligmail.com>
2025-05-20 08:48:22 -0500

    Resolucion de Ejercicios

--- src/main/ejercicio2/RespuestasOptimizadas.java
+++ src/main/ejercicio2/RespuestasOptimizadas.java
@@ -1,30 +1,49 @@
 package main.ejercicio2;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RespuestasOptimizadas {
     // El estudiante debe escribir aquí las versiones optimizadas y las
     // complejidades
 
     // 1. Versión optimizada de tieneDuplicados (Complejidad anotada en la variable)
-    public static String tieneDuplicadosComplejidad = ""; // Ej: "O(n)"
+    public static String tieneDuplicadosComplejidad = "O(n)"; // Ej: "O(n)"
 
     public static boolean tieneDuplicadosOpt(int[] arr) {
-        // TODO: implementar versión eficiente
+        Set<Integer> vistos = new HashSet<>();
+        for (int n : arr) {
+            if (vistos.contains(n)) {
+                return true;
+            }
+            vistos.add(n);
+        }
         return false;
     }
 
     // 2. Versión optimizada de contarMayores (Complejidad anotada en la variable)
-    public static String contarMayoresComplejidad = ""; // Ej: "O(n)"
+    public static String contarMayoresComplejidad = "O(n)"; // Ej: "O(n)"
 
     public static int contarMayoresOpt(int[] arr, int x) {
-        // TODO: implementar versión eficiente
-        return 0;
+        int contador = 0;
+        for (int n : arr) {
+            if (n >= x) {
+                contador++;
+            }
+        }
+        return contador;
     }
 
     // 3. Versión optimizada de encontrarMaximo (Complejidad anotada en la variable)
-    public static String encontrarMaximoComplejidad = ""; // Ej: "O(n)"
+    public static String encontrarMaximoComplejidad = "O(n)"; // Ej: "O(n)"
 
     public static int encontrarMaximoOpt(int[] arr) {
-        // TODO: implementar versión eficiente
-        return 0;
+        int max = Integer.MIN_VALUE;
+        for (int n : arr) {
+            if (n > max) {
+                max = n;
+            }
+        }
+        return max;
     }
 }
